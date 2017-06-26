94865eaa3b7da2326d644020b3f328bc7fc319e9
dws14159 <19504699+dws14159@users.noreply.github.com>
2017-06-26 16:13:17 +0100

    Added details on how to fix 'UNC paths are not supported'

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// UNC paths are not supported.  Defaulting to Windows directory.
+// To fix this, go to the project Properties -> Debug, change Working directory to somewhere on a local drive.
+// Can't check this in; CSGitCack.csproj.user is a .gitignored file
+
 namespace CSGitCack
 {
     class Program
